1bdc7318a96e7ef6cb138b04e8cc241bddd23878
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-10-07 22:14:26 +0300

    add local storage for tasks

--- src/Contexts/taskListContext.tsx
+++ src/Contexts/taskListContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, ReactNode } from "react";
+import React, { useState, createContext, ReactNode, useEffect } from "react";
 import { TaskProps, TaskListType } from "./typesContext/taskType";
 
 interface ChildrenProps {
@@ -10,74 +10,52 @@ export const TaskListContext = createContext<TaskListType | null>(null);
 export const TaskListContextProvider: React.FC<ChildrenProps> = ({
   children,
 }) => {
-  const [taskList, setTaskList] = useState<TaskProps[]>([
-    {
-      id: 1,
-      title: "Example task",
-      done: false,
-      categorie: "Home",
-      color: "#FF9C9C",
-    },
-    {
-      id: 2,
-      title: "Example task 2",
-      done: false,
-      categorie: "Home",
-      color: "#FF9C9C",
-    },
-    {
-      id: 3,
-      title: "Example task 3",
-      done: true,
-      categorie: "Home",
-      color: "#FF9C9C",
-    },
-  ]);
+  const [taskList, setTaskList] = useState<TaskProps[]>(
+    JSON.parse(localStorage.getItem("taskList") || "[]")
+  );
   const [doneTasks, setDoneTasks] = useState<TaskProps[]>(
-    taskList.filter((task: TaskProps) => task.done == true)
+    taskList.filter((task: TaskProps) => task.done === true)
   );
   const [notDoneTasks, setNotDoneTasks] = useState<TaskProps[]>(
-    taskList.filter((task: TaskProps) => task.done == false)
+    taskList.filter((task: TaskProps) => task.done === false)
   );
 
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+    setDoneTasks(taskList.filter((task: TaskProps) => task.done === true));
+    setNotDoneTasks(taskList.filter((task: TaskProps) => task.done === false));
+  }, [taskList]);
+
   const addTask = (task: TaskProps) => {
-    taskList.push(task);
-    setTaskList([...taskList]);
-    console.log(taskList);
-    setNotDoneTasks([...notDoneTasks, task]);
+    const updatedTaskList = [...taskList, task];
+    setTaskList(updatedTaskList);
   };
 
   const checkTask = (id: number) => {
-    taskList.filter((task: TaskProps) => {
+    const updatedTaskList = taskList.map((task: TaskProps) => {
       if (task.id === id) {
         task.done = !task.done;
-        setTaskList([...taskList]);
-        setDoneTasks(taskList.filter((task: TaskProps) => task.done == true));
-        setNotDoneTasks(
-          taskList.filter((task: TaskProps) => task.done != true)
-        );
       }
+      return task;
     });
+    setTaskList(updatedTaskList);
   };
+
   const editTask = (id: number, newTask: TaskProps) => {
-    const index = taskList.findIndex((task: TaskProps) => task.id === id);
-
-    if (index !== -1) {
-      taskList[index] = newTask;
-
-      setTaskList([...taskList]);
-      setDoneTasks(taskList.filter((task: TaskProps) => task.done));
-      setNotDoneTasks(taskList.filter((task: TaskProps) => !task.done));
-    }
+    const updatedTaskList = taskList.map((task: TaskProps) => {
+      if (task.id === id) {
+        return newTask;
+      }
+      return task;
+    });
+    setTaskList(updatedTaskList);
   };
 
   const deleteTask = (id: number) => {
-    const index = taskList.findIndex((task: TaskProps) => task.id === id);
-    taskList.splice(index, 1);
-    setTaskList([...taskList]);
-    console.log(taskList);
-    setDoneTasks(taskList.filter((task: TaskProps) => task.done === true));
-    setNotDoneTasks(taskList.filter((task: TaskProps) => task.done !== true));
+    const updatedTaskList = taskList.filter(
+      (task: TaskProps) => task.id !== id
+    );
+    setTaskList(updatedTaskList);
   };
 
   return (
