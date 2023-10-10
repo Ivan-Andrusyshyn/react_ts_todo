0bbf30dc3015489e763d32687a84de7d480e812e
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-10-11 00:02:43 +0300

    add

--- src/Components/AddTask/index.tsx
+++ src/Components/AddTask/index.tsx
@@ -1,20 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import * as S from "./styles";
 import Add from "../../Img/add.svg";
 import { AddType } from "../../Contexts/typesContext/addType";
 import { AddContext } from "../../Contexts/addContext";
+import { CategoriesContext } from "../../Contexts/categoriesContext";
+import { CategorieContextType } from "../../Contexts/typesContext/categoriesType";
 
 const AddTask: React.FC = () => {
   const { setShowAdd } = useContext(AddContext) as AddType;
-
+  const { categList } = useContext(CategoriesContext) as CategorieContextType;
+  const [isDisable, setIsDisable] = useState(false);
   function handleClick() {
     setShowAdd(true);
   }
-
+  useEffect(() => {
+    if (categList.length === 0) {
+      setIsDisable(true);
+    } else {
+      setIsDisable(false);
+    }
+  }, [categList]);
   return (
-    <S.Container onClick={handleClick}>
+    <S.Container onClick={handleClick} disabled={isDisable}>
       <S.Icon src={Add} />
-      <S.Text>Add a task</S.Text>
+      <S.Text>{isDisable ? "First create a category" : "Add a task"}</S.Text>
     </S.Container>
   );
 };
--- src/Components/AddTask/styles.ts
+++ src/Components/AddTask/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.button`
   height: 50px;
   width: 80vw;
   border: 1px solid #ccc;
