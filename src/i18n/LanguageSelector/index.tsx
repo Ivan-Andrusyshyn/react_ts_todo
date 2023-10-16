7d4711e7a3cc1e50dc79e50fe8e7ca894dc72c94
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-10-17 00:39:36 +0300

    a

--- src/i18n/LanguageSelector/index.tsx
+++ src/i18n/LanguageSelector/index.tsx
@@ -32,7 +32,7 @@ function LanguageSelector() {
       >
         <S.imgBtn
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/1200px-Flag_of_Ukraine.svg.png"
-          alt="Urk"
+          alt="Ukr"
         />
       </S.LanguageButton>
     </S.BtnWraper>
